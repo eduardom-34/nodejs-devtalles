f58bd88ec801edcfefead61720a2fc7b7523f7c2
CesarLinux <eduardomelchor34@gmail.com>
2026-05-15 21:27:30 -0600

    Tarea - Contador de palabras

---
+++ 01-fundamentos/app3.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const content = fs.readFileSync('README.md', 'utf8');
+
+const words = content.split(" ");
+
+ // const reactWordCount = words.filter((word) => word.toLocaleLowerCase().includes('react')).length;
+
+ const reactWordCount = content.match(/react/gi ?? []).length;
+
+console.log("Palabras:", words.length);
+console.log('Palabras React:', reactWordCount );
