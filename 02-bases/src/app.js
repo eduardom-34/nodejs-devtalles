5063f1873deabda2e33e06488cdfcba377462643
CesarLinux <eduardomelchor34@gmail.com>
2026-05-27 21:17:36 -0600

    Factory Functions - Introducción

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -4,15 +4,16 @@
 // require('./js-foundation/02-destructuring');
 // console.log(emailTemplate);
 // const { getUserById } = require('./js-foundation/03-callbacks')
-const { getUserById } = require('./js-foundation/04-arrow')
+// const { getUserById } = require('./js-foundation/04-arrow')
+const { getUserById } = require('./js-foundation/05-factory')
  
 
-const id = 1;
-getUserById(id, function (error, user) {
-  if (error) {
-    throw new Error( error );
-  }
+// const id = 1;
+// getUserById(id, function (error, user) {
+//   if (error) {
+//     throw new Error( error );
+//   }
 
-  console.log({user});
+//   console.log({user});
 
-});
+// });
