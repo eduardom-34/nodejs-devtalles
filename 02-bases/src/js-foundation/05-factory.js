2a29b47ea2566ffd8a45cbe8c2f0476a28613671
CesarLinux <eduardomelchor34@gmail.com>
2026-05-28 23:23:57 -0600

    Factory Functions - Necesidad

--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -1,16 +1,16 @@
 
-
-
+const { v4: uuidv4} = require('uuid');
+const getAge = require('get-age');
 
 
 const buildPerson = ({ name, birthdate }) => {
 
   return {
-    id: new Date().getTime(),
+    id: uuidv4(),
     name: name,
     birthdate: birthdate,
-    age: new Date().getFullYear() - new Date(birthdate).getFullYear(),
-  }
+    age: getAge(birthdate),
+  };
 }
 
 const obj = { name: "John", birthdate: "1985-10-21" };
